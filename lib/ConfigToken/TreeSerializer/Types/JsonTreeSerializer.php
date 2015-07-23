7019a13e758bbc48bfa9c2d5efa6933925f9c685
Andrei Liutec <andrei@liutec.ro>
2015-07-23 20:01:25 +0300

    PHP 5.3.3 compatibility

--- lib/ConfigToken/TreeSerializer/Types/JsonTreeSerializer.php
+++ lib/ConfigToken/TreeSerializer/Types/JsonTreeSerializer.php
@@ -32,12 +32,23 @@ class JsonTreeSerializer extends AbstractTreeSerializer
 
     public static function deserialize($string)
     {
+        static $errors = array(
+            JSON_ERROR_DEPTH           => 'Maximum stack depth exceeded',
+            JSON_ERROR_STATE_MISMATCH  => 'Underflow or the modes mismatch',
+            JSON_ERROR_CTRL_CHAR       => 'Unexpected control character found',
+            JSON_ERROR_SYNTAX          => 'Syntax error, malformed JSON',
+            JSON_ERROR_UTF8            => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+        );
+
         $data = json_decode($string, true);
-        if ((!isset($data)) && (json_last_error() != JSON_ERROR_NONE)) {
+
+        $errorCode = json_last_error();
+        if ((!isset($data)) && ($errorCode != JSON_ERROR_NONE)) {
+            $errorMsg = array_key_exists($errorCode, $errors) ? $errors[$errorCode] : "Unknown error ({$errorCode})";
             throw new TreeSerializerSyntaxException(
                 sprintf(
                     'Unable to deserialize json tree: %s',
-                    json_last_error_msg()
+                    $errorMsg
                 )
             );
         }
