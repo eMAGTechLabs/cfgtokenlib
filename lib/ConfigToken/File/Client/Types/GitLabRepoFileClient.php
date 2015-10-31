6d68c5ce100c1a2ac691f635e621bc7293bc40f8
Andrei Liutec <andrei.liutec@emag.ro>
2015-10-31 09:42:38 +0200

    GitLab repo file client

---
+++ lib/ConfigToken/File/Client/Types/GitLabRepoFileClient.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ConfigToken\File\Client\Types;
+
+
+use ConfigToken\File\Client\Exceptions\ReadException;
+use ConfigToken\File\ConnectionSettings\ConnectionSettingsInterface;
+use ConfigToken\File\ConnectionSettings\Types\GitRepoFileClientConnectionSettings;
+
+class GitLabRepoFileClient extends AbstractFileClient
+{
+    /**
+     * Get the unique id of the implementation.
+     * @return string
+     */
+    public static function getId()
+    {
+        return 'gitlab';
+    }
+
+    /**
+     * Return a new instance of the connection settings implementation class.
+     *
+     * @param ConnectionSettingsInterface|null $connectionSettings
+     * @return GitRepoFileClientConnectionSettings
+     */
+    public static function makeConnectionSettings(ConnectionSettingsInterface $connectionSettings = null)
+    {
+        return new GitRepoFileClientConnectionSettings($connectionSettings);
+    }
+
+    /**
+     * Read the contents of the given file.
+     *
+     * @param string $fileName
+     * @param boolean|true $relativeToPath
+     * @param boolean|true $updatePath
+     * @throws ReadException
+     * @return string
+     */
+    public function readFile($fileName, $relativeToPath = true, $updatePath = true)
+    {
+        throw new ReadException($fileName, sprintf('%s not implemented.', static::getClassName()));
+    }
+}
---
+++ lib/ConfigToken/File/ConnectionSettings/Types/GitLabRepoFileClientConnectionSettings.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ConfigToken\File\ConnectionSettings\Types;
+
+
+class GitLabRepoFileClientConnectionSettings extends GitRepoFileClientConnectionSettings
+{
+    const API_URL = 'url';
+    const API_TOKEN = 'token';
+}
